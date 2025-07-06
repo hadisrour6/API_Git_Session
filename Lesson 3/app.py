2e1e25353c339ec7eb0fce50e5578438a0e686ae
Hadi Srour <hadisrour@cmail.carleton.ca>
2025-07-06 12:20:22 -0400

    first flask app

---
+++ Lesson 3/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__) #This creates our app
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
